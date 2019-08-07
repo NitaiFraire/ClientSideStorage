a952960b0051abdbb3743a9a490e8e640fdd731b
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-08-07 11:13:33 -0500

    creando la base de datos

--- js/app.js
+++ js/app.js
@@ -10,3 +10,28 @@ const formulario = document.querySelector('form'),
       sintomas = document.querySelector('#sintomas'),
       citas = document.querySelector('#citas'),
       headingAdministra = document.querySelector('#administra');
+
+
+// esperar por el DOM ready
+document.addEventListener('DOMContentLoaded', () => {
+
+    // crer db
+    let crearDB = window.indexedDB.open('citas');
+
+    // si hay error enviarlo a ala consola
+    crearDB.onerror = function(){
+
+        console.log('hubo error');
+    }
+
+    // si no hay errores, mostrar en conola y asignar la base de datos
+    crearDB.onsuccess = function(){
+
+        console.log('toodo listo');
+        // asignar a la base de datos
+        DB = crearDB.result;
+        console.log(DB);
+    }
+
+
+});
