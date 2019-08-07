8b6c9d1e7d0d39a4815ec90380c742abcd81c9b8
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-08-07 11:06:55 -0500

    creando variables de la interfaz

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let DB;
+
+// Selectores de la interfaz
+const formulario = document.querySelector('form'),
+      mascota = document.querySelector('#mascota'),
+      cliente = document.querySelector('#cliente'),
+      telefono = document.querySelector('#telefono'),
+      fecha = document.querySelector('#fecha'),
+      hora = document.querySelector('#hora'),
+      sintomas = document.querySelector('#sintomas'),
+      citas = document.querySelector('#citas'),
+      headingAdministra = document.querySelector('#administra');
